0a0444f13021d0d736021a99dc65281a06595f25
colleenxyu <colleenelise.yu@gmail.com>
2026-08-25 08:41:36 +0000

    app updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,14 @@ def dashboard():
 
     st.title("Household Consumption Dashboard")
 
-    st.subheader ("Purchase Data")
-
     st.sidebar.image ("Plattered Logo.png")
-    st.sidebar.header("Purchase Data Filter")
-    
+   
 
+    st.sidebar.title("Page Navigation")
+    st.sidebar.page_link ("streamlit_app.py", label ="Meat Marketing")
+    st.sidebar.page_link ("Vegetable_Marketing.py", label ="Vegetable Marketing Dashboard")
+
+    st.sidebar.title("Purchase Data Filter")
     # LOAD DATA
     purchasedate_df = pd.read_csv("Purchase_Date.csv")
     purchasedur_df = pd.read_csv("Purchase_Duration.csv")
