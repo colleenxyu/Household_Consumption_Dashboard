95fd94791b28753143d6f51f3fc45f51732b3e98
colleenxyu <colleenelise.yu@gmail.com>
2026-08-25 08:52:32 +0000

    page navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,8 @@ def dashboard():
    
 
     st.sidebar.title("Page Navigation")
-    st.sidebar.page_link ("streamlit_app.py", label ="Meat Marketing")
-    st.sidebar.page_link ("Vegetable_Marketing.py", label ="Vegetable Marketing Dashboard")
+    st.sidebar.page_link ("pages/streamlit_app.py", label ="Meat Marketing")
+    st.sidebar.page_link ("pages/Vegetable_Marketing.py", label ="Vegetable Marketing Dashboard")
 
     st.sidebar.title("Purchase Data Filter")
     # LOAD DATA
